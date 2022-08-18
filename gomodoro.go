8c57b8dac5ad5d338238a63eb8e4a65acc9efb04
Gabriel Zöller <g.zoeller@avm.de>
2022-08-18 17:52:54 +0200

    add showing timer in systray

--- gomodoro.go
+++ gomodoro.go
@@ -8,12 +8,17 @@ import (
 	"github.com/getlantern/systray"
 	"github.com/godbus/dbus"
 	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/ttf"
 )
 
 var work *time.Timer
 var pause *time.Timer
+var tick *time.Timer
 var workdur time.Duration
 var pausedur time.Duration
+var tickdur time.Duration
+var time_left time.Duration
 var windows []*sdl.Window
 
 func main() {
@@ -21,12 +26,18 @@ func main() {
 		panic(err)
 	}
 	defer sdl.Quit()
+	if err := ttf.Init(); err != nil {
+		panic(err)
+	}
+	defer ttf.Quit()
+	updateIcon("off")
 	systray.Run(onReady, onExit)
 }
 
 func onReady() {
 	workdur = 50 * time.Minute
 	pausedur = 10 * time.Minute
+	tickdur = time.Minute
 
 	systray.SetTitle("Gomodoro")
 	systray.SetTooltip("Gomodoro Timer")
@@ -88,13 +99,18 @@ func unpauseScreen() {
 }
 
 func startSession() {
+	pause = nil
+	time_left = workdur
 	work = time.AfterFunc(workdur, startPause)
+	tick = time.AfterFunc(tickdur, ticked)
 	notify("Work Started", "")
 	playMusic()
 	unpauseScreen()
+	updateIcon(time_left_fmt())
 }
 
 func stopSession() {
+	time_left = 0
 	notify("Session Stopped", "")
 	if work != nil && !work.Stop() {
 		<-work.C
@@ -102,14 +118,23 @@ func stopSession() {
 	if pause != nil && !pause.Stop() {
 		<-pause.C
 	}
+	if tick != nil && !tick.Stop() {
+		<-tick.C
+	}
+	work = nil
+	pause = nil
+	tick = nil
 	unpauseScreen()
 }
 
 func startPause() {
+	work = nil
+	time_left = pausedur
 	notify("Pause Started", "")
 	pause = time.AfterFunc(pausedur, startSession)
 	pauseMusic()
 	pauseScreen()
+	updateIcon(time_left_fmt())
 }
 
 func pauseMusic() {
@@ -144,3 +169,51 @@ func notify(title string, message string) {
 		panic(call.Err)
 	}
 }
+
+func time_left_fmt() string {
+	status := fmt.Sprintf("%02.0f", time_left.Minutes())
+	return status
+}
+
+func ticked() {
+	time_left -= tickdur
+	tick.Reset(tickdur)
+	updateIcon(time_left_fmt())
+}
+
+func updateIcon(status string) {
+	var color sdl.Color
+	if work != nil {
+		color.R = 0
+		color.G = 255
+		color.B = 0
+		color.A = 255
+	} else if pause != nil {
+		color.R = 255
+		color.G = 0
+		color.B = 0
+		color.A = 255
+	} else {
+		color.R = 128
+		color.G = 128
+		color.B = 128
+		color.A = 255
+	}
+	font, _ := ttf.OpenFont("./Kenney High Square.ttf", 22)
+	text, _ := font.RenderUTF8Blended(status, color)
+
+	square, _ := sdl.CreateRGBSurface(0, 22, 22, 32, 0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF)
+	text.BlitScaled(&text.ClipRect, square, &square.ClipRect)
+
+	dynamic_icon := make([]byte, 16384)
+	rwops, err := sdl.RWFromMem(dynamic_icon)
+	if err != nil {
+		fmt.Printf("error while creating rwops: %s\n", err)
+	}
+	err = img.SavePNGRW(square, rwops, 0)
+	if err != nil {
+		fmt.Printf("error while writing: %s\n", err)
+	}
+	systray.SetIcon(dynamic_icon)
+}
+
